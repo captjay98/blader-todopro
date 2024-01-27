09ba083d30abcf456528bcf6b099b5991be649fe
captjay98 <captjay98@gmail.com>
2024-01-27 23:50:31 +0100

    added composable for showing Todos in the dashboard todo-list component

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Todo;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        view()->composer('todo-list', function ($view) {
+            $view->with('todos', Todo::take(6)->get());
+        });
     }
 }
