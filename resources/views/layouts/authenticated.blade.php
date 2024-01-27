e10fc9fdddc2cd866190d9730ad243117e6203a2
captjay98 <captjay98@gmail.com>
2024-01-27 23:48:42 +0100

    added authenticated layout updated guest and navigation layouts and deleted app layout

--- resources/views/layouts/authenticated.blade.php
+++ resources/views/layouts/authenticated.blade.php
@@ -16,9 +16,21 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased bg-black w-screen h-screen">
-    <div class="w-full h-full bg-blue-900/20">
-        {{ $slot }}
+<body class="font-sans antialiased bg-black w-screen h-screen overflow-hidden">
+    <div class="pt-10 m-auto w-full h-full md:pt-2 bg-blue-900/20">
+        @include('layouts.navigation')
+        @if (isset($header))
+            <header class="bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+        <div class="overflow-y-auto px-3 w-full h-full">
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
     </div>
 </body>
 
